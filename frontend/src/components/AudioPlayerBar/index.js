105b815709228f9dd1d394c7ea508377d4f8be04
Wilson Chen <wilsonchen901@gmail.com>
2022-10-16 22:47:32 -0400

    work on audio navbar

--- frontend/src/components/AudioPlayerBar/index.js
+++ frontend/src/components/AudioPlayerBar/index.js
@@ -3,77 +3,30 @@ import sample from '../../assets/temp_music/sample.mp3';
 
 const AudioPlayerBar = () => {
     return ( 
-        <div className='audio-bar-container'>
-            <div className='audio-bar'>
-                <div className='playback-buttons'>
-                    <button className='prev-track'>
-    
-                    </button>
-                    
-                    <button className='play-track'>
-    
-                    </button>
-    
-                    <button className='next-track'>
-                        
-                    </button>
-    
-                    <button className='shuffle-track'>
-    
-                    </button>
-    
-                    <button className='repeat-track'>
-    
-                    </button>
+        <div className='audio-bar'>
+            <button className='prev-track'></button>
+            <button className='play-pause'></button>
+            <button className='next-track'></button>
+            <button className='repeat-track'></button>
+
+            <div className='track-timeline'>
+                <div className='track-start'></div>
+                <div className='track-progress'></div>
+                <div className='track-end'></div>
+            </div>
+            
+            <div className='track-volume'></div>
+
+            <div className='track-info'>
+                <div className='track-details'>
+                    <a href="#">Link to artist</a>
+                    <span>Track Title</span>
                 </div>
-    
-                <div className='track-progress-bar'>
-                    <div className='track-start'>
-    
-                    </div>
-    
-                    <div className='track-progress'>
-                        <audio controls>
-                            <source src={sample}
-                            type='audio/mpeg'
-                            />
-                        </audio>
-                    </div>
-    
-                    <div className='track-end'>
-    
-                    </div>
-                </div>
-    
-    
-                <div className='track-info'>
-                    <div className='track-title'>
-                        Title
-                    </div>
-    
-                    <div className='track-artist'>
-                        Artist
-                    </div>
-                </div>
-    
-                <div className='track-socials'>
-                    <button className='track-like'>
-    
-                    </button>
-    
-                    <button className='track-follow'>
-    
-                    </button>
-    
-                    <button className='track-playlist'>
-    
-                    </button>
-                </div>
-    
+
+                <div className='track-socials'></div>
             </div>
         </div>
-
-     );
+    );
 }
  
 export default AudioPlayerBar;
