7068db4271ee35ca0e3642830ea8951a2e36daf1
Wilson Chen <wilsonchen901@gmail.com>
2022-10-17 10:08:22 -0400

    create play pause button logic

--- frontend/src/components/AudioPlayerBar/index.js
+++ frontend/src/components/AudioPlayerBar/index.js
@@ -1,11 +1,39 @@
 import './AudioPlayerBar.css';
 import sample from '../../assets/temp_music/sample.mp3';
+import play from '../../assets/icons/play-solid.svg';
+import pause from '../../assets/icons/pause-solid.svg';
+import { useEffect, useState } from 'react';
 
 const AudioPlayerBar = () => {
+    
+    const playUrl = `url(${play})`;
+    const pauseUrl = `url(${pause})`;
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [playPause, setPlayPause] = useState(playUrl);
+
+
+    const handlePlay = (e) => {
+        console.log(isPlaying);
+        setIsPlaying(!isPlaying);
+
+        isPlaying ? setPlayPause(pauseUrl) : setPlayPause(playUrl);
+    }
+
+    
+    const buttonBackground = {
+        backgroundImage: playPause
+    }
+
     return ( 
         <div className='audio-bar'>
+            <audio className='track'>
+                <source src={sample} type='audio/mpeg'/>
+            </audio>
             <button className='prev-track'></button>
-            <button className='play-pause'></button>
+            <button className='play-pause'
+            style={buttonBackground}
+            onClick={(e) => handlePlay(e)}
+            ></button>
             <button className='next-track'></button>
             <button className='repeat-track'></button>
 
