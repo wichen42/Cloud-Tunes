f984de42d493deec134e8c79b890b76ab23a0013
Wilson Chen <wilsonchen901@gmail.com>
2022-10-17 12:44:44 -0400

    add volume button

--- frontend/src/components/AudioPlayerBar/index.js
+++ frontend/src/components/AudioPlayerBar/index.js
@@ -2,16 +2,27 @@ import './AudioPlayerBar.css';
 import sample from '../../assets/temp_music/sample.mp3';
 import play from '../../assets/icons/play-solid.svg';
 import pause from '../../assets/icons/pause-solid.svg';
+import volLow from '../../assets/icons/volume-low-solid.svg';
+import volHigh from '../../assets/icons/volume-high-solid.svg';
+import volMute from '../../assets/icons/volume-xmark-solid.svg';
 import { useEffect, useRef, useState } from 'react';
 
 const AudioPlayerBar = ({tracks}) => {
     
     const playUrl = `url(${play})`;
     const pauseUrl = `url(${pause})`;
+    const volLowUrl = `url(${volLow})`;
     const [isPlaying, setIsPlaying] = useState(false);
     const [playPause, setPlayPause] = useState(playUrl);
     const audioPlayer = useRef();
     const [currentSong, setCurrentSong] = useState(sample);
+    const trackList = tracks.map(track => track.trackUrl);
+    const [volBackground, setVolbackground] = useState(volLowUrl);
+
+
+    console.log(trackList);
+    console.log(volLowUrl);
+    console.log(playUrl);
 
     const handlePlay = (e) => {
         const prevState = isPlaying;
@@ -27,14 +38,18 @@ const AudioPlayerBar = ({tracks}) => {
         }
     }
 
+    const volumeBackground = {
+        backgroundImage: volBackground
+    }
     
     const buttonBackground = {
         backgroundImage: playPause
     }
 
+
     return ( 
         <div className='audio-bar'>
-            <audio src={currentSong} ref={audioPlayer}></audio>
+            <audio src={trackList[0]} ref={audioPlayer}></audio>
             <button className='prev-track'></button>
             <button className='play-pause'
             style={buttonBackground}
@@ -54,7 +69,7 @@ const AudioPlayerBar = ({tracks}) => {
                 <div className='track-end'>3:23</div>
             </div>
             
-            <div className='track-volume'></div>
+            <button className='track-volume' style={volumeBackground}></button>
 
             <div className='track-info'>
                 <div className='track-details'>
