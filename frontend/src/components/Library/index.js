0ac12128232b0783a7cbeba24b638f7a857b12f5
Wilson Chen <wilsonchen901@gmail.com>
2022-10-19 12:32:45 -0400

    basic css for library

--- frontend/src/components/Library/index.js
+++ frontend/src/components/Library/index.js
@@ -1,14 +1,45 @@
+import { useSelector } from 'react-redux';
+import * as trackActions from '../../store/track';
 import './Library.css';
 
 const Library = () => {
 
-    
+    const tracks = useSelector(trackActions.getTracks);
 
     return ( 
         <div className='library-container'>
-            <div>
                 <h1>All Tracks</h1>
-
+            <div>
+                <ul>
+                    {tracks.map(track => {
+                       return(
+                        <li key={track.id}
+                        className="track-item"
+                        >
+                            <div className='track-detail-container'>
+                                <div>Title:</div>
+                                <div>{track.title}</div>
+                            </div>
+                            <div className='track-detail-container'>
+                                <div>Username:</div>
+                                <div>{track.username}</div>
+                            </div>
+                            <div className='track-detail-container'>
+                                <div>Genre:</div>
+                                <div>{track.genre}</div>
+                            </div>
+                            <div className='track-detail-container'>
+                                <div>Description</div>
+                                <div>{track.description}</div>
+                            </div>
+                            <div className='track-detail-container'>
+                                <div>URL:</div>
+                                <div>{track.trackUrl}</div>
+                            </div>
+                        </li>
+                       ) 
+                    })}
+                </ul>
             </div>
         </div>
      );
