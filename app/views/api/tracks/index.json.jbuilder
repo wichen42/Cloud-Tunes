701bf94969bc28ee53a15aeb38e1fdb31b3505bf
Wilson Chen <wilsonchen901@gmail.com>
2022-10-19 16:38:02 -0400

    work on track component styling

--- app/views/api/tracks/index.json.jbuilder
+++ app/views/api/tracks/index.json.jbuilder
@@ -1,6 +1,6 @@
 @tracks.each do |track|
   json.set! track.id do
-    json.extract! track, :id, :title, :username, :genre, :description, :user_id
+    json.extract! track, :id, :title, :username, :genre, :description, :user_id, :created_at
     json.trackUrl track.track.url
     json.imageUrl track.image.url
   end
--- frontend/src/components/TrackDisplayBar/index.js
+++ frontend/src/components/TrackDisplayBar/index.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import './TrackDisplay.css';
 
-const TrackDisplay = (track) => {
+const TrackDisplay = ({track}) => {
 
     const [imageUrl, setImageUrl] = useState("https://cloud-tunes-dev.s3.amazonaws.com/pexels-pixabay-159868.jpg");
+
+    console.log(track.title);
 
     useEffect(() => {
         if (track.imageUrl) {
@@ -18,10 +20,31 @@ const TrackDisplay = (track) => {
             </div>
             <div className='track-details container'>
                 <div className='track-info'>
-                    <div className='track-artist'>{track.username}</div>
-                    <div className='track-title'>{track.title}</div>
+                    <div className='track-name-title'>
+                        <div className='track-artist'>{track.username}</div>
+                        <div className='track-title'>{track.title}</div>
+                    </div>
+                    <div className='track-create-genre'>
+                        <div className='track-time'>{Math.floor(Math.random()*12)} months ago</div>
+                        <div className='track-genre'><div>#{track.genre}</div></div>
+                    </div>
                 </div>
-                <div></div>
+
+                <div className='track-description'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis harum, sunt, iure laboriosam voluptates quaerat reprehenderit maiores asperiores necessitatibus quae molestiae maxime alias ex itaque neque dolorem placeat! Quis, amet?
+                </div>
+
+                <div className='track-comments'>
+                    <input type="text"
+                    placeholder='Write a comment'
+                    />
+                </div>
+
+                <div className='track-buttons-container'>
+                    <div className='track-likes'>Like</div>
+                    <div className='track-links'>Copy Link</div>
+                    <div className='track-comment'>Comments</div>
+                </div>
             </div>
         </div>
      );
