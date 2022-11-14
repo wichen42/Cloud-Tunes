b608f9846e44408b9f91a85779e32ad113b4cfb0
Wilson Chen <wilsonchen901@gmail.com>
2022-11-14 17:49:00 -0500

    work on playlist buttons

--- frontend/src/components/PlaylistItem/index.js
+++ frontend/src/components/PlaylistItem/index.js
@@ -1,9 +1,25 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './PlaylistItem.css';
 
 const PlaylistItem = ({track}) => {
 
     const [showButtons, setShowButtons] = useState(false);
+
+    const handlePlay = (e) => {
+        e.preventDefault();
+        console.log("Play");
+    }
+
+    const handleLike = (e) => {
+        e.preventDefault();
+        console.log("Like");
+    }
+
+    const handleFollow = (e) => {
+        e.preventDefault();
+        console.log("Follow");
+    }
 
 
     return ( 
@@ -14,6 +30,17 @@ const PlaylistItem = ({track}) => {
             >
                 {showButtons && 
                     <div className='discover-playlist-buttons'>
+                        <div className='discover-playlist-buttons-container'>
+                            <div className='discover-playlist-play'
+                            onClick={(e) => handlePlay(e)} 
+                            ><FontAwesomeIcon icon="fa-solid fa-play" /></div>
+                            <div className='discover-playlist-like'
+                            onClick={(e) => handleLike(e)}
+                            ><FontAwesomeIcon icon="fa-solid fa-heart" /></div>
+                            <div className='discover-playlist-follow'
+                            onClick={(e) => handleFollow(e)}
+                            ><FontAwesomeIcon icon="fa-solid fa-user-plus" /></div>
+                        </div>
                     </div>
                 }
                     <span className='playlist-track-name'>{track.username} - </span> <span>&nbsp;{track.title}</span>
