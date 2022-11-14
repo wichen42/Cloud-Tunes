ff321a13ee89b7a42e1ae5f75aef0f3fc25ddbdf
Wilson Chen <wilsonchen901@gmail.com>
2022-11-14 18:11:07 -0500

    work on playlist style

--- frontend/src/components/Playlist/index.js
+++ frontend/src/components/Playlist/index.js
@@ -7,17 +7,27 @@ const Playlist = ({tracks}) => {
     const playlistItems = tracks.map(track => <PlaylistItem track={track} key={track.id}/>);
 
     return ( 
-        <div className='discover-playlist-container'>
-            <div className='discover-playlist-image-container'>
-                <img src="https://cloud-tunes-dev.s3.amazonaws.com/playlist-background-square.jpg"
-                className='discover-playlist-image'
-                />
+        <div className='playlist-box'>
+            <div className='discover-playlist-container-header'>
+                More of what you like
             </div>
 
-            <div className='discover-playlist-details-container'>
-                    {playlistItems}
+            <div className='discover-playlist-container'>
+            <div className='discover-playlist-container-body'>
+                <div className='discover-playlist-image-container'>
+                    <img src="https://cloud-tunes-dev.s3.amazonaws.com/playlist-background-square.jpg"
+                    className='discover-playlist-image'
+                    />
+                </div>
+    
+                <div className='discover-playlist-details-container'>
+                        {playlistItems}
+                </div>
             </div>
+
         </div> 
+        </div>
+
 
     );
 }
