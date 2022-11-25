dcddb7b237d32ea7c47da43a08c31e746db62198
Wilson Chen <wilsonchen901@gmail.com>
2022-11-25 14:47:38 -0500

    work on searchbar functionality

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,22 +1,43 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import * as userActions from '../../store/users';
+import * as trackActions from '../../store/track';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Searchbar.css';
+import { useEffect } from 'react';
 
 const SearchBar = () => {
 
     const [value, setValue] = useState("");
-    
-    console.log(value);
+    const userList = useSelector(userActions.getUsers);
+    const trackList = useSelector(trackActions.getTracks);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const allData = [];
+        userList.map((el) => allData.push(el.username));
+        trackList.map((el) => allData.push(el.title));
+        console.log(allData);
+    }, [userList, trackList])
 
     return ( 
-        <div className='header-search'>
-            <input type="text" value={value} 
-            onChange={(e) => setValue(e.target.value)}
-            className="search"
-            />
-            <button className='search-button'>
-                <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" size='s'/>
-            </button>
+        <div className='header-search-container'>
+            <div className='header-search'>
+                <input type="text" value={value} 
+                onChange={(e) => setValue(e.target.value)}
+                className="search"
+                />
+                <button className='search-button'>
+                    <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" size='sm'/>
+                </button>
+            </div>
+            <div className='search-dropdown'>
+                {userList.map((searchItem) => (
+                    <div>
+                        {searchItem.username ? searchItem.username : searchItem.title} 
+                    </div>
+                ))}
+            </div>
         </div>
      );
 }
