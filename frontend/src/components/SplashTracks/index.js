bdfb296ef657fbc923e3d0f5e8a1ec6208813011
Wilson Chen <wilsonchen901@gmail.com>
2023-01-19 12:59:09 -0500

    hard set splash tracks instead of shuffleArr

--- frontend/src/components/SplashTracks/index.js
+++ frontend/src/components/SplashTracks/index.js
@@ -3,11 +3,22 @@ import * as trackActions from '../../store/track';
 import { useSelector } from 'react-redux';
 import { shuffleArr } from '../../Util';
 import SplashTrackItem from '../SplashTrackItem';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const SplashTracks = () => {
     
     const tracks = useSelector(trackActions.getTracks);
-    const trackList = shuffleArr(tracks, 10);
+    const [trackList, setTrackList] = useState([]);
+
+    useEffect(() => {
+        setTrackList([
+            tracks[1], tracks[7], tracks[15], tracks[21], tracks[8], 
+            tracks[30], tracks[66], tracks[76], tracks[99], tracks[102],  
+        ])
+    }, [tracks]);
+
+    // const trackList = shuffleArr(tracks, 10);
     const trackItem = trackList.map(track => {
         return <SplashTrackItem track={track} />
     });
